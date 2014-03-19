91e7aced1296e368858aa48cee9ce62bb921b3d6
hgentry <harrison.c.gentry@gmail.com>
2014-03-19 11:05:32 -0400

    Fractions!

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value / 2048.0;
+  inner.textContent = "1" + "/" + 2048/tile.value;
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
